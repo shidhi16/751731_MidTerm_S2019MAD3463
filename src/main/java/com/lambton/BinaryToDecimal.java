6ead1a92f2ee6b81005a873dfa57b30f87c8c96e
Shivani <shivanidhiman1619@gmail.com>
2019-05-31 19:11:12 -0400

    [Shivani]: Reverse done

---
+++ src/main/java/com/lambton/BinaryToDecimal.java
@@ -0,0 +1,9 @@
+package com.lambton;
+
+public class BinaryToDecimal
+{
+    public static void main(String[] args)
+    {
+        int x = 12345;
+    }
+}
---
+++ src/main/java/reverse.java
@@ -0,0 +1,14 @@
+
+public class reverse
+
+{
+    public static void mai()
+    {
+        String s = "Lambton";
+
+        for (int i= s.length()-1; i>=0; i--){
+
+            System.out.println(s.charAt(i));
+        }
+    }
+}
