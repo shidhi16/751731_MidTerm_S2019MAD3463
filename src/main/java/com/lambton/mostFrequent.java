e9af24e1f8313efb263d71538ee5e24619a2d9ce
Shivani <shivanidhiman1619@gmail.com>
2019-05-31 20:09:54 -0400

    [Shivani]: Most Frequent

---
+++ src/main/java/com/lambton/mostFrequent.java
@@ -0,0 +1,27 @@
+package com.lambton;
+
+public class mostFrequent {
+    public static void main(String[] args) {
+        String s = "This is a java program. Java is a popular programming language, created in 1995. It is owned by Oracle, and more than 3 billion devices run Java";
+        int[] freq = new int[s.length()];
+        char maxChar= s.charAt(0);
+
+        int i,j,max;
+        char String []= s.toCharArray();
+
+        for
+        (i=0; i < String.length; i++)
+        {
+            freq[i] = 1;
+            for
+            {j= i+1; j<String.length; j++)
+                {
+                    if String[i]== String[j] && String
+                }
+
+
+    }
+
+
+       }
+}
